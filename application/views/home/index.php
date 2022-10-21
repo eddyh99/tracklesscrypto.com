2d5926a1b21badb61469face61a03ec9ac17ea65
adipurnama <imadeadipurnamayasa@gmail.com>
2022-10-21 12:04:52 +0800

    update layout

---
+++ application/views/home/index.php
@@ -0,0 +1,256 @@
+<nav class="navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="<?= base_url(); ?>assets/images/tc-crypto-new.png">
+        </a>
+        <a href="https://tracklessproject.com" class="bg-trackless me-5">
+            <svg width="57" height="53" viewBox="0 0 57 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M26.908 1.37204C27.8784 0.750986 29.1214 0.750986 30.0918 1.37204L54.7011 17.122C56.0749 18.0012 56.4758 19.8276 55.5966 21.2013C54.7174 22.575 52.8911 22.9759 51.5174 22.0967L49.1717 20.5955V44.2188C49.1717 48.568 45.646 52.0938 41.2967 52.0938H15.703C11.3538 52.0938 7.828 48.568 7.828 44.2188V20.5955L5.48239 22.0967C4.10868 22.9759 2.28235 22.575 1.40317 21.2013C0.523991 19.8276 0.924893 18.0012 2.29861 17.122L26.908 1.37204ZM19.1483 24.0391C18.3328 24.0391 17.6717 24.7001 17.6717 25.5156C17.6717 26.3311 18.3328 26.9922 19.1483 26.9922H37.8514C38.6669 26.9922 39.328 26.3311 39.328 25.5156C39.328 24.7001 38.6669 24.0391 37.8514 24.0391H19.1483ZM17.6717 31.4219C17.6717 30.6064 18.3328 29.9453 19.1483 29.9453H27.0233C27.8388 29.9453 28.4999 30.6064 28.4999 31.4219C28.4999 32.2374 27.8388 32.8984 27.0233 32.8984H19.1483C18.3328 32.8984 17.6717 32.2374 17.6717 31.4219ZM19.1483 35.8516C18.3328 35.8516 17.6717 36.5126 17.6717 37.3281C17.6717 38.1436 18.3328 38.8047 19.1483 38.8047H30.9608C31.7763 38.8047 32.4374 38.1436 32.4374 37.3281C32.4374 36.5126 31.7763 35.8516 30.9608 35.8516H19.1483Z"
+                    fill="#00DD9C" />
+            </svg>
+        </a>
+    </div>
+</nav>
+<!-- Mashead header-->
+<header class="slider">
+    <div class="container-fluid px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-12">
+                <!-- Mashead text and app badges-->
+                <div class="mb-5 mb-lg-0 text-lg-start">
+                    <h1 class="text-tracklesscrypto-line mb-3 f-poppins">Get your own
+                        exchange platform
+                        <img src="<?= base_url(); ?>assets/images/line.png" alt="line">
+                    </h1>
+                </div>
+            </div>
+            <div class="col-12 col-lg-5 list-crypto-top f-poppins mt-2 mt-lg-5">
+                <div class="d-flex flex-row">
+                    <div class="p-2 align-items-center d-flex">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">Free of charge</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="p-2 align-items-center d-flex">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">Shared crypto order book & Liquidity</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="p-2 align-items-center d-flex">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">Including a collecting & payment gateway</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="p-2 align-items-center d-flex">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">The platform will be host for free on our servers</p>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-sm-none d-lg-inline">
+                <!-- Masthead device mockup feature-->
+                <div class="masthead-device-mockup text-center">
+                    <img class="img-fluid" src="<?= base_url(); ?>assets/images/chart.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Quote/testimonial aside-->
+<section class="bg-content">
+    <div class="container-fluid px-5 py-5">
+        <div class="row align-items-center cube-content my-5">
+            <div class="col-lg-8">
+                <span class="title-cube f-poppins">How can we make it possible?</span>
+                <p class="mt-3">
+                    <span class="text-green">TracklessCrypto</span> combines a unique FIAT payment and collection system
+                    with a shared
+                    Crypto order
+                    book.<br>
+                    Thanks to this combination we give to the clients the possibility of being able to open their own
+                    exchange platform , completely free of charge, without any worries about liquidity and/or license
+                    and in order to give its users the possibility to keep, buy and sell Crypto with FIAT anonymously,
+                    without KYC or AML.
+                </p>
+            </div>
+            <div class="col-lg-4 d-none d-sm-none d-lg-inline">
+                <div class="px-5 px-sm-0 text-center">
+                    <img class="cobe-img" src="<?= base_url(); ?>assets/images/cube.png" alt="cube" />
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center cube-content my-5">
+            <div class="col-lg-5 d-none d-sm-none d-lg-inline">
+                <div class="px-5 px-sm-0">
+                    <img class="img-fluid" src="<?= base_url(); ?>assets/images/sheal.png" alt="cube" />
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <p class="mt-3">
+                    Thanks to the merger of the <span class="text-green">TracklessCrypto</span> shared order book and
+                    the TracklessMoney payment and
+                    collection gateway, we provide a turnkey solution for your exchange platform. You will only have to
+                    buy a domain name, which will be hosted for free on our servers, and choose your graphics.<br>
+                    We recommend, for greater development and expansion, to insert a 0.05% fee on the purchase and sale
+                    of takers and makers. The 0.05% fee will be divided as follows:<br>
+                    0.01% Referral Link (Suggested);<br>
+                    0.02% TracklessCrypto platform (Mandatory);<br>
+                    0.02% Gain (Suggested).
+                </p>
+            </div>
+        </div>
+
+        <div class="row align-items-center d-flex justify-content-center cube-content">
+            <div class="col-12 col-sm-10 col-md-8 p-0 p-lg-3 my-5">
+                <div class="box-form px-4 px-lg-5 py-5 f-inter">
+                    <h2 class="f-inter mb-4">Are you interested?</h2>
+                    <a href="#">Apply here</a>
+                    <form class="row g-3 needs-validation mt-4" novalidate>
+                        <div class="col-lg-12 form-tracklesscrypto">
+                            <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="email" placeholder="Enter your email" required>
+                        </div>
+                        <div class="col-lg-12 form-tracklesscrypto">
+                            <label for="company" class="form-label">Company</label>
+                            <input type="text" class="form-control" id="company"
+                                placeholder="Enter your company website">
+                        </div>
+                        <div class="col-lg-12 form-tracklesscrypto">
+                            <label for="instagram" class="form-label">Instagram address</label>
+                            <input type="text" class="form-control" id="instagram"
+                                placeholder="Enter your instagram address">
+                        </div>
+                        <div class="col-lg-12 form-tracklesscrypto">
+                            <label for="twitter" class="form-label">Twitter address </label>
+                            <input type="text" class="form-control" id="twitter"
+                                placeholder="Enter your twitter address">
+                        </div>
+                        <div class="col-lg-12 form-tracklesscrypto">
+                            <label for="facebook" class="form-label">Facebook address </label>
+                            <input type="text" class="form-control" id="facebook"
+                                placeholder="Enter your facebook address">
+                        </div>
+                        <div class="col-lg-12 form-tracklesscrypto">
+                            <label for="linkedin" class="form-label">Linkedin address </label>
+                            <input type="text" class="form-control" id="linkedin" placeholder="Linkedin address ">
+                        </div>
+                        <div class="col-12 text-center mt-5">
+                            <button class="btn btn-tracklesscrypto py-2 px-5" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="row align-items-center my-5">
+            <div class="col-12 text-center">
+                <div class="powered mb-3">
+                    <span class="f-monserat">Powered by </span>
+                    <img src="<?= base_url(); ?>assets/images/tracklesscrypto.png" alt="tracklesscrypto">
+                </div>
+                <div class="sponsor d-flex justify-content-center flex-wrap">
+                    <div class="sponsor-cover d-flex align-items-center mx-3 my-2">
+                        <img src="<?= base_url(); ?>assets/images/sponsor1.png" alt="">
+                    </div>
+                    <div class="sponsor-cover d-flex align-items-center mx-3 my-2">
+                        <img src="<?= base_url(); ?>assets/images/sponsor2.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="bg-footer-trackless py-3">
+    <div class="container-fluid px-md-5">
+        <div class="container">
+            <div class="bg-imagebox">
+                <img src="<?= base_url() ?>assets/images/trackless-fit.png" alt="" class="footer-logo-center">
+            </div>
+            <div class="d-flex justify-content-around">
+                <div class="col-4">
+                    <img src="<?= base_url(); ?>assets/images/footer-logo.png" class="footer-logo">
+                    <div class="d-flex flex-wrap icon-sosmed">
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/fb-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/twitter-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/ig-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/c-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/linkedin-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/tiktok-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/youtube-icon.png" class="p-1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">&nbsp;</div>
+                <div class="col-4 footer-contact">
+                    <h2>Contact us</h2>
+                    <span>Telegramchanel</span>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-wrap justify-content-end footer-menus" translate="no">
+            <a href="https://tracklessproject.com" class="">TracklessProject</a> |
+            <a href="https://tracklessmail.com" class="active">TracklessMail</a> |
+            <a href="https://tracklesschat.com" class="">TracklessChat</a> |
+            <a href="https://tracklesscompany.com" class="">TracklessCompany</a> |
+            <a href="https://tracklessbank.com" class="">TracklessBank</a> |
+            <a href="https://tracklesscrypto.com" class="">TracklessCrypto</a> |
+            <a href="https://tracklessmoney.com" class="">TracklessMoney</a>
+        </div>
+    </div>
+</footer>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
+    tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content modal-trackless">
+            <div class="modal-header border-0">
+                <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path
+                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                    </svg>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                This email will be used only for password recovery and all the information, included your email, are
+                encrypted SHA256 on NP blockchain and obfuscated with CRYPTONOTE protocol
+            </div>
+        </div>
+    </div>
+</div>
