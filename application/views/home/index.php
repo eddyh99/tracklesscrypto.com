2a5b7a4af33847da9a56dfe515418db8aadcbb64
Adi Purnama <imadeadipurnamayasa@gmail.com>
2023-02-20 12:05:04 +0800

    Update Deskripsi

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -5,9 +5,7 @@
         </a>
         <a href="https://tracklessproject.com" class="bg-trackless me-5">
             <svg width="57" height="53" viewBox="0 0 57 53" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M26.908 1.37204C27.8784 0.750986 29.1214 0.750986 30.0918 1.37204L54.7011 17.122C56.0749 18.0012 56.4758 19.8276 55.5966 21.2013C54.7174 22.575 52.8911 22.9759 51.5174 22.0967L49.1717 20.5955V44.2188C49.1717 48.568 45.646 52.0938 41.2967 52.0938H15.703C11.3538 52.0938 7.828 48.568 7.828 44.2188V20.5955L5.48239 22.0967C4.10868 22.9759 2.28235 22.575 1.40317 21.2013C0.523991 19.8276 0.924893 18.0012 2.29861 17.122L26.908 1.37204ZM19.1483 24.0391C18.3328 24.0391 17.6717 24.7001 17.6717 25.5156C17.6717 26.3311 18.3328 26.9922 19.1483 26.9922H37.8514C38.6669 26.9922 39.328 26.3311 39.328 25.5156C39.328 24.7001 38.6669 24.0391 37.8514 24.0391H19.1483ZM17.6717 31.4219C17.6717 30.6064 18.3328 29.9453 19.1483 29.9453H27.0233C27.8388 29.9453 28.4999 30.6064 28.4999 31.4219C28.4999 32.2374 27.8388 32.8984 27.0233 32.8984H19.1483C18.3328 32.8984 17.6717 32.2374 17.6717 31.4219ZM19.1483 35.8516C18.3328 35.8516 17.6717 36.5126 17.6717 37.3281C17.6717 38.1436 18.3328 38.8047 19.1483 38.8047H30.9608C31.7763 38.8047 32.4374 38.1436 32.4374 37.3281C32.4374 36.5126 31.7763 35.8516 30.9608 35.8516H19.1483Z"
-                    fill="#00DD9C" />
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M26.908 1.37204C27.8784 0.750986 29.1214 0.750986 30.0918 1.37204L54.7011 17.122C56.0749 18.0012 56.4758 19.8276 55.5966 21.2013C54.7174 22.575 52.8911 22.9759 51.5174 22.0967L49.1717 20.5955V44.2188C49.1717 48.568 45.646 52.0938 41.2967 52.0938H15.703C11.3538 52.0938 7.828 48.568 7.828 44.2188V20.5955L5.48239 22.0967C4.10868 22.9759 2.28235 22.575 1.40317 21.2013C0.523991 19.8276 0.924893 18.0012 2.29861 17.122L26.908 1.37204ZM19.1483 24.0391C18.3328 24.0391 17.6717 24.7001 17.6717 25.5156C17.6717 26.3311 18.3328 26.9922 19.1483 26.9922H37.8514C38.6669 26.9922 39.328 26.3311 39.328 25.5156C39.328 24.7001 38.6669 24.0391 37.8514 24.0391H19.1483ZM17.6717 31.4219C17.6717 30.6064 18.3328 29.9453 19.1483 29.9453H27.0233C27.8388 29.9453 28.4999 30.6064 28.4999 31.4219C28.4999 32.2374 27.8388 32.8984 27.0233 32.8984H19.1483C18.3328 32.8984 17.6717 32.2374 17.6717 31.4219ZM19.1483 35.8516C18.3328 35.8516 17.6717 36.5126 17.6717 37.3281C17.6717 38.1436 18.3328 38.8047 19.1483 38.8047H30.9608C31.7763 38.8047 32.4374 38.1436 32.4374 37.3281C32.4374 36.5126 31.7763 35.8516 30.9608 35.8516H19.1483Z" fill="#00DD9C" />
             </svg>
         </a>
     </div>
@@ -29,19 +27,7 @@
                 <div class="d-flex flex-row">
                     <div class="p-2 align-items-center d-flex">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">Free of charge</p>
-                </div>
-                <div class="d-flex flex-row">
-                    <div class="p-2 align-items-center d-flex">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div>
                     <p class="mt-2 mt-sm-1 m-0 mt-lg-2">Shared crypto order book & Liquidity</p>
@@ -49,9 +35,7 @@
                 <div class="d-flex flex-row">
                     <div class="p-2 align-items-center d-flex">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div>
                     <p class="mt-2 mt-sm-1 m-0 mt-lg-2">Including a collecting & payment gateway</p>
@@ -59,12 +43,10 @@
                 <div class="d-flex flex-row">
                     <div class="p-2 align-items-center d-flex">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-                                stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="#00DD9C" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div>
-                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">The platform will be host for free on our servers</p>
+                    <p class="mt-2 mt-sm-1 m-0 mt-lg-2">The platform will be host on our servers</p>
                 </div>
             </div>
             <div class="col-lg-7 d-none d-sm-none d-lg-inline">
@@ -107,15 +89,11 @@
             </div>
             <div class="col-lg-7">
                 <p class="mt-3">
-                    Thanks to the merger of the <span class="text-green">TracklessCrypto</span> shared order book and
-                    the TracklessMoney payment and
-                    collection gateway, we provide a turnkey solution for your exchange platform. You will only have to
-                    buy a domain name, which will be hosted for free on our servers, and choose your graphics.<br>
-                    We recommend, for greater development and expansion, to insert a 0.05% fee on the purchase and sale
-                    of takers and makers. The 0.05% fee will be divided as follows:<br>
-                    0.01% Referral Link (Suggested);<br>
-                    0.02% TracklessCrypto platform (Mandatory);<br>
-                    0.02% Gain (Suggested).
+                    Thanks to the merger of the <span class="text-green">TracklessCrypto</span> shared order book and the TracklessMoney payment and collection gateway, we provide a turnkey solution for your exchange platform. You will only have to choose the name of the platform, which will be hosted on our servers, and choose your graphics.<br>
+                    We recommend, for greater development and expansion, to insert a 0.05% fee on the purchase and sale of takers and makers. The 0.05% fee will be divided as follows:<br>
+                    0.02% TracklessCrypto platform for buy and sell (Mandatory);<br>
+                    0.02% For buy and sell Gain of your platform (Suggested);<br>
+                    0.01% Referral Link (Suggested).
                 </p>
             </div>
         </div>
@@ -125,56 +103,47 @@
                 <div class="box-form px-4 px-lg-5 py-5 f-inter">
 
                     <?php if (@isset($_SESSION["failed"])) { ?>
-                    <div class="col-12 alert alert-danger alert-dismissible fade show" role="alert">
-                        <span class="notif-login f-poppins"><?= @$_SESSION["failed"] ?></span>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
+                        <div class="col-12 alert alert-danger alert-dismissible fade show" role="alert">
+                            <span class="notif-login f-poppins"><?= @$_SESSION["failed"] ?></span>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
                     <?php } ?>
                     <?php if (@isset($_SESSION["success"])) { ?>
-                    <div class="col-12 alert alert-success alert-dismissible fade show" role="alert">
-                        <span class="notif-login f-poppins"><?= @$_SESSION["success"] ?></span>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
+                        <div class="col-12 alert alert-success alert-dismissible fade show" role="alert">
+                            <span class="notif-login f-poppins"><?= @$_SESSION["success"] ?></span>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
                     <?php } ?>
                     <h2 class="f-inter mb-4">Are you interested?</h2>
                     <a href="#">Apply here</a>
-                    <form class="row g-3 needs-validation mt-4" action="<?= base_url() ?>auth/mailproses" method="POST"
-                        onsubmit="return validate()">
-                        <input type="hidden" id="token" name="<?php echo $this->security->get_csrf_token_name(); ?>"
-                            value="<?php echo $this->security->get_csrf_hash(); ?>">
+                    <form class="row g-3 needs-validation mt-4" action="<?= base_url() ?>auth/mailproses" method="POST" onsubmit="return validate()">
+                        <input type="hidden" id="token" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
                         <div class="col-lg-12 form-tracklesscrypto">
                             <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="email" placeholder="Enter your email"
-                                name="email" required>
+                            <input type="text" class="form-control" id="email" placeholder="Enter your email" name="email" required>
                         </div>
                         <div class="col-lg-12 form-tracklesscrypto">
                             <label for="company" class="form-label">Company</label>
-                            <input type="text" class="form-control" id="company"
-                                placeholder="Enter your company website" name="company">
+                            <input type="text" class="form-control" id="company" placeholder="Enter your company website" name="company">
                         </div>
                         <div class="col-lg-12 form-tracklesscrypto">
                             <label for="instagram" class="form-label">Instagram address</label>
-                            <input type="text" class="form-control" id="instagram"
-                                placeholder="Enter your instagram address" name="instagram">
+                            <input type="text" class="form-control" id="instagram" placeholder="Enter your instagram address" name="instagram">
                         </div>
                         <div class="col-lg-12 form-tracklesscrypto">
                             <label for="twitter" class="form-label">Twitter address </label>
-                            <input type="text" class="form-control" id="twitter" name="twitter"
-                                placeholder="Enter your twitter address">
+                            <input type="text" class="form-control" id="twitter" name="twitter" placeholder="Enter your twitter address">
                         </div>
                         <div class="col-lg-12 form-tracklesscrypto">
                             <label for="facebook" class="form-label">Facebook address </label>
-                            <input type="text" class="form-control" id="facebook" name="facebook"
-                                placeholder="Enter your facebook address">
+                            <input type="text" class="form-control" id="facebook" name="facebook" placeholder="Enter your facebook address">
                         </div>
                         <div class="col-lg-12 form-tracklesscrypto">
                             <label for="linkedin" class="form-label">Linkedin address </label>
-                            <input type="text" class="form-control" id="linkedin" name="linkedin"
-                                placeholder="Linkedin address ">
+                            <input type="text" class="form-control" id="linkedin" name="linkedin" placeholder="Linkedin address ">
                         </div>
                         <div class="col-12 text-center mt-5">
-                            <button id="btnconfirm" class="btn btn-tracklesscrypto py-2 px-5"
-                                type="submit">Submit</button>
+                            <button id="btnconfirm" class="btn btn-tracklesscrypto py-2 px-5" type="submit">Submit</button>
                         </div>
                     </form>
                 </div>
@@ -257,16 +226,13 @@
 </footer>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
-    tabindex="-1">
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content modal-trackless">
             <div class="modal-header border-0">
                 <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-x-lg" viewBox="0 0 16 16">
-                        <path
-                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                     </svg>
                 </button>
             </div>
